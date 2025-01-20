87f8ff152b2882b02f70bc2f607a7a27fac5baf5
ernkny <eren.kinay.98@windowslive.com>
2025-01-21 01:15:52 +0300

    New Services are added

---
+++ Unbiased.Dashboard.Api/Program.cs
@@ -0,0 +1,26 @@
+using Unbiased.ApiGateway.Infrastructure.DataAccess.Connections;
+using Unbiased.Shared.ExceptionHandler.Middleware.Concrete.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("UnbiasedSqlConnection");
+builder.Services.AddTransient<UnbiasedSqlConnection>(provider => new UnbiasedSqlConnection(connectionString!));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseMiddleware<GlobalExceptionMiddleware>();
+app.UseMiddleware<GlobalActivityLogMiddleware>();
+app.UseMiddleware<ApiKeyAuthorizeMiddleware>();
+app.MapControllers();
+
+app.Run();
